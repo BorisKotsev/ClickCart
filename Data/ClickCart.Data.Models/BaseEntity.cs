48364b85445f269dc133ab704d124de063137bdf
bobiko23 <55994876+bobiko23@users.noreply.github.com>
2023-05-09 18:01:03 +0300

    Add project files.

---
+++ Data/ClickCart.Data.Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickCart.Data.Models
+{
+    public abstract class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+    }
+}
